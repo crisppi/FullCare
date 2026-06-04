45eb8e5e8d181e701ae5ffc1c7058fa0e9b635d7
Accert Consult <66572787+crisppi@users.noreply.github.com>
2026-06-04 17:57:02 -0300

    edit pasta

--- edit_internacao_EA.php
+++ edit_internacao_EA.php
@@ -1,8 +1,1 @@
-<?php
-include_once("check_logado.php");
-include_once("globals.php");
-
-$id_internacao = filter_input(INPUT_GET, 'id_internacao', FILTER_VALIDATE_INT) ?: 1;
-
-header('Location: ' . $BASE_URL . 'edit_internacao.php?id_internacao=' . (int)$id_internacao . '&section=gestao#div_evento');
-exit;
+<?php require_once __DIR__ . '/pages/edit/edit_internacao_EA.php';
