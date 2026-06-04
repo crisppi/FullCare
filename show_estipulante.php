47927f1af8a3e6cbb746d4996efe2384b0f44007
Accert Consult <crisppi@gmail.com>
2026-06-04 16:10:45 -0300

    show - em pastas proprias.

--- show_estipulante.php
+++ show_estipulante.php
@@ -1,11 +1,1 @@
-<?php
-include_once("check_logado.php");
-include_once("globals.php");
-
-$id_estipulante = filter_input(INPUT_GET, "id_estipulante", FILTER_VALIDATE_INT);
-$redirectUrl = $id_estipulante
-    ? $BASE_URL . "estipulantes/editar/" . (int)$id_estipulante
-    : $BASE_URL . "estipulantes";
-
-header("Location: " . $redirectUrl);
-exit;
+<?php require_once __DIR__ . '/pages/show/show_estipulante.php';
--- show_seguradora.php
+++ show_seguradora.php
@@ -1,11 +1,1 @@
-<?php
-include_once("check_logado.php");
-include_once("globals.php");
-
-$id_seguradora = filter_input(INPUT_GET, "id_seguradora", FILTER_VALIDATE_INT);
-$redirectUrl = $id_seguradora
-    ? $BASE_URL . "seguradoras/editar/" . (int)$id_seguradora
-    : $BASE_URL . "seguradoras";
-
-header("Location: " . $redirectUrl);
-exit;
+<?php require_once __DIR__ . '/pages/show/show_seguradora.php';
